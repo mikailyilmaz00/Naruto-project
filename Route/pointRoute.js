65990dc00bc0690f552b9d5c5344b8dd2c900ca9
mikailyilmaz00 <mikail17yz@gmail.com>
2025-04-10 15:37:28 +0200

    massive changes of details

--- Route/pointRoute.js
+++
@@ -1,7 +0,0 @@
-
-const { updatePointsAndRank } = require('../Controller/pointController');
-const express = require('express');
-const router = express.Router();
-router.route('/pointRank').put(updatePointsAndRank);
-
-module.exports = router;
